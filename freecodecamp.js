c4cddd8d3ffd48bf382d612ac4f061af3b38267d
Vit Onischenko <slonik0775@gmail.com>
2022-07-05 17:44:58 +0300

    Create a variable called myData and set it to equal the first value of myArray using bracket notation

--- freecodecamp.js
+++ freecodecamp.js
@@ -1,5 +1,2 @@
-// Setup
-const lastName = "Lovelace";
-
-// Only change code below this line
-const lastLetterOfLastName = lastName[lastName.length  - 1]; // Change this line
+const myArray = [50, 60, 70];
+const myData = myArray[0];
