a3eb060e909180e6ded0ae472a35a4917c1e740d
Vit Onischenko <slonik0775@gmail.com>
2022-07-06 22:31:42 +0300

    Chaining If Else Statements

--- freecodecamp.js
+++ freecodecamp.js
@@ -1,13 +1,19 @@
-function testElseIf(val) {
-    if (val > 10) {
-      return "Greater than 10";
-    }
-  
-   else if (val < 5) {
-      return "Smaller than 5";
-    } else{
-  
-    return "Between 5 and 10";
+function testSize(num) {
+    // Only change code below this line
+  if (num < 5) {
+     return 'Tiny'
+  } else if (num < 10) {
+     return 'Small'
+  } else if (num < 15) {
+     return 'Medium'
+  } else if(num < 20){
+    return 'Large'
+  } else {
+    return 'Huge'
   }
+  
+    return "Change Me";
+    // Only change code above this line
   }
-  testElseIf(7);
+  
+  testSize(7);
