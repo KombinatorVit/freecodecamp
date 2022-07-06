85e9898deca9f97747953e62b4b03b94fd297f92
Vit Onischenko <slonik0775@gmail.com>
2022-07-06 16:36:30 +0300

    Use Conditional Logic with If Statements

--- freecodecamp.js
+++ freecodecamp.js
@@ -1,16 +1,10 @@
-// Setup
-let sum = 0;
-
-function addThree() {
-  sum = sum + 3;
-}
-
-// Only change code below this line
-
-function addFive(){
-  sum = sum + 5;
-}
-// Only change code above this line
-
-addThree();
-addFive();
+function trueOrFalse(wasThatTrue) {
+    // Only change code below this line
+  
+  if(wasThatTrue){
+    return 'Yes, that was true'
+  }
+  return 'No, that was false'
+    // Only change code above this line
+  
+  }
