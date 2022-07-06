79c1db1b43618eeb440d01af790cb0f45dc8a767
Vit Onischenko <slonik0775@gmail.com>
2022-07-06 16:25:47 +0300

    Understanding Undefined Value returned from a Function

--- freecodecamp.js
+++ freecodecamp.js
@@ -1,19 +1,16 @@
 // Setup
-const myGlobal = 10;
+let sum = 0;
 
-function fun1() {
-oopsGlobal = 5;
+function addThree() {
+  sum = sum + 3;
 }
 
+// Only change code below this line
+
+function addFive(){
+  sum = sum + 5;
+}
 // Only change code above this line
 
-function fun2() {
-  var output = "";
-  if (typeof myGlobal != "undefined") {
-    output += "myGlobal: " + myGlobal;
-  }
-  if (typeof oopsGlobal != "undefined") {
-    output += " oopsGlobal: " + oopsGlobal;
-  }
-  console.log(output);
-}
+addThree();
+addFive();
