b785f459bb57797b9c429bc9f5a8c82b484e85f4
Vit Onischenko <slonik0775@gmail.com>
2022-07-06 22:40:53 +0300

    Selecting from Many Options with Switch Statements

--- freecodecamp.js
+++ freecodecamp.js
@@ -1,21 +1,25 @@
-function golfScore(par, strokes) {
+function caseInSwitch(val) {
+    let answer = "";
     // Only change code below this line
   
-    if (strokes === 1) return "Hole-in-one!";
   
-    else if ((strokes - par) <= -2) return 'Eagle';
+  switch(val){
+  case 1:
+  answer ='alpha'
+  break;
+  case 2:
+  answer ='beta'
+  break;
+  case 3:
+  answer ='gamma'
+  break;
+  case 4:
+  answer ='delta'
+  break;
   
-    else if ((strokes - par) === -1) return 'Birdie';
-  
-    else if (strokes === par) return 'Par';
-  
-    else if ((strokes - par) === 1) return 'Bogey';
-  
-    else if ((strokes - par) === 2) return 'Double Bogey';
-  
-    else return 'Go Home!';
+  }
     // Only change code above this line
+    return answer;
   }
   
-  // Change these values to test
-  golfScore(5, 1);
+  caseInSwitch(1);
