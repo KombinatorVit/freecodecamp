2cd600ef12e2b1c6b40c5ee3a9be669ac172077a
Vit Onischenko <slonik0775@gmail.com>
2022-07-09 18:04:49 +0300

    Using Objects for Lookups

--- freecodecamp.js
+++ freecodecamp.js
@@ -1,8 +1,22 @@
-const myDog = {
-    "name": "Happy Coder",
-    "legs": 4,
-    "tails": 1,
-    "friends": ["freeCodeCamp Campers"]
-  };
+// Setup
+function phoneticLookup(val) {
+    var result = "";
   
-  myDog.bark="woof";
+    var lookup = {
+      "alpha": "Adams",
+      "bravo": "Boston",
+      "charlie": "Chicago",
+      "delta": "Denver",
+      "echo": "Easy",
+      "foxtrot": "Frank"
+    };
+    // Only change code below this line
+  
+    result = lookup[val];
+  
+    // Only change code above this line
+    return result;
+  }
+  
+  // Change this value to test
+  phoneticLookup("charlie");
