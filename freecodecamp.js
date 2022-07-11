a7243c5926d133f18b5b7be98ea290d9dee8eb14
Vit Onischenko <slonik0775@gmail.com>
2022-07-11 22:40:26 +0300

    Compare Scopes of the var and let Keywords

--- freecodecamp.js
+++ freecodecamp.js
@@ -1,2 +1,13 @@
 
 
+function checkScope() {
+  "use strict";
+    let i = "function scope";
+    if (!true) {
+      i = "block scope";
+      console.log("Block scope i is: ", i);
+    } else {
+    console.log("Function scope i is: ", i);
+    return i;
+    }
+  }
