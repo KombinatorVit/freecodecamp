17562bb3b6df2508852739efa49b5d24ee887f6d
Vit Onischenko <slonik0775@gmail.com>
2022-07-18 13:45:00 +0300

    Generate Random Fractions with JavaScript

--- freecodecamp.js
+++ freecodecamp.js
@@ -1,11 +1,8 @@
-const stats = {
-  max: 56.78,
-  standard_deviation: 4.34,
-  median: 34.54,
-  mode: 23.87,
-  min: -0.75,
-  average: 35.85
-};
+function randomFraction() {
 
-// Only change code below this line
-const half = ({max, min}) => ((max + min) / 2.0);// Only change code above this line
+  // Only change code below this line
+
+  return Math.random();
+
+  // Only change code above this line
+}
